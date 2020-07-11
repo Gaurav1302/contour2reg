9be6bdbf073dd98f4fff4152d4bebd7eaa1ca364
gaurav1302 <gauravjain13298@gmail.com>
2020-07-11 18:39:42 +0530

    allow mesh to be specified

--- Generate/blender_generate.py
+++ Generate/blender_generate.py
@@ -3,34 +3,44 @@ import os
 import subprocess
 import random
 
-print("==========================================")
-print("Generating ", cfg.num, " Random Meshes")
-print("==========================================")
 
-for i in range(cfg.num):
-    # Create folder for this sample
-    outputDir = os.path.join(cfg.Data_path,str(i+1))
-    if (not os.path.isdir(outputDir)):
-        os.mkdir(outputDir)
+if cfg.MeshExists:
+    # Check if the mesh is an'.stl' file
+    if not cfg.MeshPath.endswith( ".stl" ):
+        raise IOError("Given mesh must be a .stl file.")
     else:
-        print("Directory already exists..")
+        print("==========================================")
+        print("Mesh Found! Skipping mesh generation.")
+        print("==========================================")
+else:
+    # Create Random Meshes
+    print("==========================================")
+    print("Generating ", cfg.num, " Random Meshes")
+    print("==========================================")
+    for i in range(cfg.num):
+        # Create folder for this sample
+        outputDir = os.path.join(cfg.Data_path,str(i+1))
+        if (not os.path.isdir(outputDir)):
+            os.mkdir(outputDir)
+        else:
+            print("Directory already exists..")
 
-    # Call blender script (generateRandomMesh.py) to generate random meshes
-    a = ["blender"]
-    a += ["--background"]
-    a += ["-noaudio"]
-    a += ["--python"]
-    a += ["generateRandomMesh.py"]
-    a += ["--"]
-    a += ["--outdir"]
-    a += [outputDir]
-    # blender --background -noaudio --python generateRandomMesh.py -- --outdir "../Data/1/"
-    p = subprocess.call( a, shell=False, timeout=None )
+        # Call blender script (generateRandomMesh.py) to generate random meshes
+        a = ["blender"]
+        a += ["--background"]
+        a += ["-noaudio"]
+        a += ["--python"]
+        a += ["generateRandomMesh.py"]
+        a += ["--"]
+        a += ["--outdir"]
+        a += [outputDir]
+        # blender --background -noaudio --python generateRandomMesh.py -- --outdir "../Data/1/"
+        p = subprocess.call( a, shell=False, timeout=None )
 
-    print("=================")
-    print("Completed ", i+1, "/", cfg.num)
-    print("=================")
+        print("=================")
+        print("Completed ", i+1, "/", cfg.num)
+        print("=================")
 
-print("==========================================")
-print("Generation Complete")
-print("==========================================")
+    print("==========================================")
+    print("Generation Complete")
+    print("==========================================")
--- Generate/config.py
+++ Generate/config.py
@@ -2,3 +2,5 @@
 Data_path = "../Data/" # Save the generated synthetic data in this folder
 num = 2
 seed = 20
+MeshExists = False
+MeshPath = "../Data/1/randomMesh.stl"
